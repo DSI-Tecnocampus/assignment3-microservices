d852ff8e5acd24fdf2f20df9bc73ff4cfe0ab467
Josep Roure Alcobé <roure@tecnocampus.cat>
2024-02-11 23:44:03 +0100

    upgraded to spring-boot-parent 3.2.0 and mysql-connector-j

--- product/src/main/resources/schema.sql
+++ product/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int NOT NULL AUTO_INCREMENT,
     `name` varchar(45) NOT NULL,
     `description` varchar(255) NOT NULL,
     PRIMARY KEY (`id`)
